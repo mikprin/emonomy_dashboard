a8d55736dd427a928fd25cfa7aa80c70a1aaf7dc
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2022-11-20 16:07:22 +0400

    Working convert script

--- shell_scripts/convert.sh
+++ shell_scripts/convert.sh
@@ -2,7 +2,7 @@
 # Convert a file to a different format
 
 target_dir=$1
-
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Convert all files in the target directory
 
@@ -14,18 +14,18 @@ do
     # Convert the file to a different format
     case $extension in
         "xls" | "XLS" | "xlsx" | "XLSX")
-            ssconvert $file ./${file%.*}.csv
+            short_filename="$(basename "$file")"
+           echo 'Converting file: ' "$short_filename"
+            ssconvert "$file" ./"$(echo "$short_filename" | cut -f1 -d'.').csv"
+            echo ssconvert "$file" "./$(echo "$short_filename" | cut -f1 -d'.').csv"
             ;;
-        # "png")
-        #     convert $file ./${file%.*}.jpg
-        #     ;;
         *)
             # If file is a directory, enter it
             if [ -d $file ]
             then
-                echo "Entering directory $file"
-                SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-                ( mkdir -p $(basename ${file}) && cd $(basename ${file}) && $SCRIPTPATH/convert.sh $file )
+                new_path=$(realpath $file)
+                echo "Entering directory $new_path"
+                ( mkdir -p "$(basename ${file})" && cd "$(basename ${file})" && "$SCRIPTPATH/convert.sh" "$new_path" )
             else
                 echo "File $file is not an xls or xlsx file"
             fi
@@ -33,6 +33,7 @@ do
     esac
 done
 
+# Old format:
 # ssconvert $1/emotions_data.xlsx ./emotions_data.csv
 # ssconvert $1/emotions_metrics.xlsx ./emotions_metrics.csv
 # ssconvert $1/statement_irt_result.xlsx ./statement_irt_result.csv
